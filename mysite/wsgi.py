015824905d717962ea8de0b1df6c2aacf03fc978
valueplusalpha <wonwooya0430@naver.com>
2018-12-15 17:30:09 +0900

    first commit

---
+++ mysite/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path = '/home/valueplusalpha/wonwoozzang'  # PythonAnywhere 계정으로 바꾸세요.
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
